e2278a621aee33398b78c08b7c62bfd35cb1d44f
matteoga95 <gallo.work95@gmail.com>
2023-01-25 23:57:43 +0100

    importato assets e home fatta

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -2,42 +2,60 @@
 <script>
 
 export default {
-    name: "AppNavbar"
+    name: "AppNavbar",
+    data() {
+        return {
+            header: '#header'
+        }
+    }
 
 }
+
+function stickyNavbar() {
+
+
+    this.header.classList.toggle("scrolled", window.pageYOffset > 0)
+    console.log(this.header.classList);
+}
+
+window.addEventListener("scroll", stickyNavbar)
+
 </script>
 
 <template>
-    <header>
+    <header id="header">
         <nav class="container-new d-flex justify-content-center flex-wrap">
             <a href="#" class="logo">Portf<span>olio.</span></a>
-            <div class="links d-flex justify-content-between">
-                <ul class="d-flex justify-content-center w-100">
+            <div class="links d-flex justify-content-between align-items-center">
+                <ul class="d-flex justify-content-start align-items-center w-100">
                     <li>
-                        <RouterLink class="nav-link" :to="{ name: 'home' }">
+                        <RouterLink class="navb-link" :to="{ name: 'home' }">
                             Home
                         </RouterLink>
 
                     </li>
                     <li>
-                        <RouterLink class="nav-link" :to="{ name: 'home' }">
+                        <RouterLink class="navb-link" :to="{ name: 'home' }">
                             About
                         </RouterLink>
                     </li>
 
                     <li>
-                        <RouterLink class="nav-link" :to="{ name: 'projects' }">
+                        <RouterLink class="navb-link" :to="{ name: 'projects' }">
                             Portfolio
                         </RouterLink>
                     </li>
                     <li>
-                        <RouterLink class="nav-link" :to="{ name: 'home' }">
+                        <RouterLink class="navb-link" :to="{ name: 'home' }">
                             Contacts
                         </RouterLink>
                     </li>
 
                 </ul>
-                <font-awesome-icon icon="fa-solid fa-moon" />
+                <div class="toggle-button d-flex align-items-center justify-content-center">
+                    <font-awesome-icon icon="fa-regular fa-moon " />
+                </div>
+
             </div>
         </nav>
 
@@ -46,7 +64,7 @@ export default {
 
 
 <style lang="scss" scoped>
-@use "../styles/general.scss";
+@import "../styles/general.scss";
 
 header {
     position: fixed;
@@ -54,19 +72,85 @@ header {
     left: 0;
     width: 100%;
     z-index: 10;
+    transition: 0.3s;
+
+
+    .logo {
+        height: 120px;
+        line-height: 120px;
+    }
+
+
+
+}
+
+.scrolled {
+    background-color: $bg-color-transparent;
+    box-shadow: 0 5px 20px 0.1px rgba(0, 0, 0, 0.1);
+
+    .logo {
+        height: 80px;
+        line-height: 80px;
+    }
 }
 
 .links {
     flex: 1;
 }
 
-.logo {
-    margin-right: 1.5rem;
-    height: 120px;
-    line-height: 120px;
-    font-family: "Abril Fatface", cursive;
-    font-size: 1.5rem;
+.navb-link {
+    display: inline-block;
+    position: relative;
+    margin: 0 1rem;
+    text-transform: uppercase;
+    font-size: 0.75rem;
+    letter-spacing: 0.5px;
+    color: $text-color;
+    font-weight: 500;
+    opacity: 0.9;
+    transition: 0.3s;
+}
+
+.navb-link:hover {
     color: $heading-color;
-    letter-spacing: 1px;
+}
+
+.navb-link:after {
+    content: "";
+    position: absolute;
+    width: 0;
+    height: 10px;
+    background-color: $main-color-1;
+    border-radius: 5px;
+    bottom: 0;
+    left: -5px;
+    z-index: -1;
+    opacity: 0.2;
+    transition: 0.3s;
+}
+
+.navb-link:hover::after {
+    width: 100%;
+    opacity: 0.2;
+}
+
+.toggle-button {
+    color: $heading-color;
+    min-width: 40px;
+    height: 40px;
+    background-color: $bg-color-2;
+    border-radius: 50%;
+    font-size: 1.1rem;
+    cursor: pointer;
+    box-shadow: 0 0 10px 0px rgba(0, 0, 0, 0.05);
+    transition: 0.3s;
+    margin-right: 1rem;
+}
+
+.toggle-button:hover {
+
+    filter: brightness(97%);
+
+
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,14 +7,148 @@ export default {
 
 <template>
 
-    <section class="container d-flex    justify-content-center">
+    <main>
+        <section class="home-area">
+            <div class="container-new d-flex ">
+                <div class="home-info ">
+                    <h3 class="sub-heading">Full-Stack Developer</h3>
+                    <h1 class="heading">Matteo Gallo</h1>
+                    <p class="text">
+                        Hi! have a nice look on my site!
+                    </p>
+                    <div class="cta d-flex align-items-center">
+                        <a href="" class="btn-f">Contact me</a>
+                        <a href="" class="btn-s">Download CV</a>
+                    </div>
+                </div>
+
+                <img src="../assets/img/man-avatar.png.jpeg" class="  person" alt="" />
+                <!-- <div class="home-image">
+
+                </div> -->
+            </div>
+        </section>
+    </main>
+    <!-- <section class="container d-flex    justify-content-center">
         <h1 class="hero-heading">hi, i'm a<br>Web Developer </h1>
         <img src="img/home.png" class="home-img" alt="">
-    </section>
+    </section> -->
 
 
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+@import "../styles/general.scss";
 
+.home-area {
+    padding: 120px 0;
+    width: 100%;
+    overflow: hidden;
+
+
+
+    .sub-heading {
+        margin-bottom: 0.7rem;
+    }
+
+    .heading {
+        font-size: 3.5rem;
+    }
+
+    .text {
+        margin: 0.8rem 0;
+    }
+}
+
+.container-new {
+
+    position: relative;
+    height: calc(100vh - 120px);
+    max-height: 600px;
+    align-items: center;
+}
+
+.cta {
+    margin-top: 3.5rem;
+
+    .btn-f {
+        margin-right: 1.5rem;
+
+
+    }
+
+}
+
+.btn-f {
+    display: inline-block;
+    padding: .9rem 1.75rem;
+    border-radius: 17px;
+    background-color: $main-color-1;
+    color: #fff;
+    font-size: 0.85rem;
+    letter-spacing: 0.4px;
+    text-transform: capitalize;
+    transition: 0.3s;
+}
+
+.btn-f:hover {
+    background-color: $main-color-dark-1;
+    color: #fff;
+}
+
+.btn-s {
+    position: relative;
+    background-color: transparent;
+    color: $main-color-1;
+    font-weight: 500;
+    font-size: 0.9rem;
+    letter-spacing: 0px;
+    border-bottom: 2.5px solid $main-color-2;
+    border-radius: 0px;
+    padding: 0.15rem 0;
+}
+
+.btn-s::before {
+    content: "";
+    position: absolute;
+    width: 0%;
+    height: 2.5px;
+    bottom: -2.5px;
+    left: 0;
+    background-color: $main-color-1;
+    transition: 0.3s ease-in-out;
+}
+
+.btn-s:hover::before {
+    width: 100%;
+}
+
+// .home-image {
+//     position: relative;
+//     width: 50%;
+//     min-height: calc(100% + 120px);
+//     background-image: linear-gradient(124deg,
+//             $main-color-1 50%,
+//             $main-color-2 50%);
+//     margin-top: -120px;
+//     border-radius: 0 0 80px 80px;
+
+
+// }
+
+.person {
+    position: absolute;
+    width: 35%;
+
+    right: 0;
+    top: 0;
+    background-image: linear-gradient(124deg,
+            $main-color-1 50%,
+            $main-color-2 50%);
+    margin-top: -120px;
+    padding: 100px;
+    padding-top: 30px;
+    border-radius: 0 0 80px 80px;
+    transform: translate(-50%);
+}
 </style>
